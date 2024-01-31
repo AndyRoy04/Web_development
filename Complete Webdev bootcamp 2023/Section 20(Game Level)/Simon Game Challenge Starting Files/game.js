a2cbbb7f4b0aca89a198831ca4c44ae1d8c6af83
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-01-31 16:41:03 +0100

    Adding animations and sounds when clicked

--- Complete Webdev bootcamp 2023/Section 20(Game Level)/Simon Game Challenge Starting Files/game.js
+++ Complete Webdev bootcamp 2023/Section 20(Game Level)/Simon Game Challenge Starting Files/game.js
@@ -1,23 +1,49 @@
 // Simon game starts here
+
 var gamePattern = [];
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var userClickedPattern = [];
 
-$(".btn").click(function(){
-    var userChosenColor = $(this).attr('id');
 
+$(".btn").click(function(){
+
+    var userChosenColor = $(this).attr('id');     
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
 
 });
 
+
 function newSequence(){
+
     var randomNumber = Math.floor(Math.random() * 4);
 
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(150).fadeIn(100);
+    // var audio = new Audio("sounds/" + randomChosenColor + ".mp3" );
+    // audio.play(); 
+    playSound(randomChosenColor);
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3" );
-    audio.play();       
+}
+
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3" );
+    audio.play() 
+}
+
+
+function animatePress(currentColor) {
+
+        $("#" + currentColor).addClass("pressed");   
+        
+        setTimeout(function(){
+            $("#" + currentColor).removeClass("pressed");
+        }, 100);
+
 }
 
