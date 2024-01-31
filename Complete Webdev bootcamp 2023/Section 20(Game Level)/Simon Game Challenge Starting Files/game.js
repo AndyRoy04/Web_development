019c21970f0bbe4d3c2e6f36a057c0dc8ae3fbb3
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-01-31 06:08:33 +0100

    jQuery to trigger a handler function.

--- Complete Webdev bootcamp 2023/Section 20(Game Level)/Simon Game Challenge Starting Files/game.js
+++ Complete Webdev bootcamp 2023/Section 20(Game Level)/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,23 @@
-alert("Hello andy");
+// Simon game starts here
+var gamePattern = [];
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
+var userClickedPattern = [];
+
+$(".btn").click(function(){
+    var userChosenColor = $(this).attr('id');
+
+
+});
+
+function newSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(150).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3" );
+    audio.play();       
+}
+
