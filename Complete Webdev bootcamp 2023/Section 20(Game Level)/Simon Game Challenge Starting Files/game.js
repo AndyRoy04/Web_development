7a57b309b4b54cd5e785af5cec3f3b078049e0d9
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-02 07:48:47 +0100

    Game complete (added a track your score field)
    The device creates a series of tones and lights and requires a user to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over.

--- Complete Webdev bootcamp 2023/Section 20(Game Level)/Simon Game Challenge Starting Files/game.js
+++ Complete Webdev bootcamp 2023/Section 20(Game Level)/Simon Game Challenge Starting Files/game.js
@@ -6,18 +6,20 @@ var userClickedPattern = [];
 
 var gameStart = false;
 var level = 0;
+var score = -1;
 
-$(document).keypress(function(){
-    gameStart = true;
-    if(gameStart){
+$(document).keypress(function () {
+    if (!gameStart) {
         $('#level-title').text("Level " + level);
+        $("#score").text("Current score : " + score);
         newSequence();
-    }   
+        gameStart = true;
+    }
 });
 
-$(".btn").click(function(){
+$(".btn").click(function () {
 
-    var userChosenColor = $(this).attr('id');     
+    var userChosenColor = $(this).attr('id');
     userClickedPattern.push(userChosenColor);
 
     playSound(userChosenColor);
@@ -25,53 +27,63 @@ $(".btn").click(function(){
 
     checkAnswer(userClickedPattern.length - 1)
 });
-
+// Check user Patter to random chosen pattern
 function checkAnswer(currentLevel) {
-    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
-        // console.log("success");
-        if(userClickedPattern.length === gamePattern.length){
-            setTimeout(function(){
+
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
                 newSequence();
             }, 1000);
         }
+    }    else {
+
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play(); 
+
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any key to restart");
         
-    } else{
-        // console.log("failure");
-    }
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#score").text("Final score : " + score);
+        startOver();
+      }
 }
-
-
-function newSequence(){
-
+// COmputer guessing function for game
+function newSequence() {
     userClickedPattern = [];
     level++;
-
+    score++;
+    $("#level-title").text("Level " + level);
+    $("#score").text("Current score : " + score);
     var randomNumber = Math.floor(Math.random() * 4);
-
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(150).fadeIn(100);
-    // var audio = new Audio("sounds/" + randomChosenColor + ".mp3" );
-    // audio.play(); 
     playSound(randomChosenColor);
-
 }
-
-
+// Function to play sound
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3" );
-    audio.play() 
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play()
 }
-
-
+// Fucntion to animate on User Click
 function animatePress(currentColor) {
 
-        $("#" + currentColor).addClass("pressed");   
-        
-        setTimeout(function(){
-            $("#" + currentColor).removeClass("pressed");
-        }, 100);
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
 
 }
-
+// Game restart
+function startOver() {
+    level = 0;
+    score = -1;
+    gamePattern = [];
+    gameStart = false;
+}
