bc892391f2ec363ac72a05393a117ab95a2ebedf
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-04 07:10:45 +0100

    starting up with NodeJS

---
+++ Complete Webdev bootcamp 2023/Section 23 (Node.js)/2.1 Using Node/index.js
@@ -0,0 +1,10 @@
+console.log("Let's run this with 'node node.js'");
+
+// function fun(number) {
+//     if (number > 10){
+//         return number - 10;
+//     }else{
+//        return fun(fun(number + 10)); 
+//     }
+// }
+// console.log(fun(10));
